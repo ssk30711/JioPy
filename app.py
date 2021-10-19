7fd5c7dc69dea86b69c8a5e7344ca47802500fda
Elvis Tony <elvistonyrency@gmail.com>
2021-10-19 15:59:06 +0530

    Removed FireTV Controls (Buggy)

--- app.py
+++ app.py
@@ -2,31 +2,16 @@ from logging import DEBUG
 from flask import Flask,render_template,stream_with_context
 import requests
 from jiolib import JioTV
-from firetv import FireTV
 from urllib.parse import unquote
 
 app = Flask(__name__)
-jio = JioTV("+918921433239", "treegrp123")
-# firetv = FireTV()
+jio = JioTV("+918921433239", "password")
+
 
 @app.route("/")
 def home():
     return render_template('player.html', pathToSource="Animal_Planet_HD.m3u8")
-    
-@app.route("/remote")
-def remote():
-    return render_template('remote.html')
 
-@app.route("/remote/key/<path>")
-def remoteKey(path):
-    result = firetv.keyPress(path.upper())
-    if(result!=""):
-        return str(path)
-    return path + "<Failed>"
-    
-@app.route("/remote/tv/<path>")
-def remoteTV(path):
-    return "tvmode"
 
 @app.route("/video-stream/<path:path>")
 def remoteVideoStream(path):
@@ -36,14 +21,6 @@ def remoteVideoStream(path):
     else:
         return "<Failed>"
 
-
-@app.route("/url-site/<path:path>")
-def remoteURL(path): 
-    result = firetv.openURI(unquote(path))
-    if(result):
-        return path
-    else:
-        return "<Failed>"
 
 @app.route("/<path:path>")
 def common_page(path):
